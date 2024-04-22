4f3c6995f96f923c332b9ee8703065ed8777b239
HighonHTML <125417055+HighonHTML@users.noreply.github.com>
2024-04-23 01:13:36 +0530

    implement useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header.jsx";
+import Main from "./Main.jsx";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log("reducer runs");
+  switch (action.type) {
+    case "dataRecieved":
+      console.log("data fetched");
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready", 
+      };
+    case "dataFailed":
+      console.log("datafailed");
+      return {
+         ...state,
+        status: "error",
+      };
+    default:
+      console.log("default");
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    async function getQuestions() {
+      try {
+        const response = await fetch("http://localhost:9000/questions");
+        const data = await response.json();
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    getQuestions();
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
 function Header() {
+  const logo = './public/logo192.png'
   return (
     <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+      <img src={logo} alt='React logo' />
       <h1>The React Quiz</h1>
     </header>
   );
